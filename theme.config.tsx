08cc4439282ef8f3cce750a6247dd3914cbfa279
Ibitoye Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2023-09-16 09:33:45 +0000

    Customize to classroomio

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,57 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <div>
+      <img
+        src={`/img/logo-512`}
+        alt={"ClassroomIO logo"}
+        width={32}
+        height={32}
+      />
+      <span>ClassroomIO</span>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/classroomio/docs',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "ClassroomIO Help Docs",
+      openGraph: {
+        images: [{ url: `/img/og-image.png` }],
+        siteName: "ClassroomIO Help Docs",
+      },
+    };
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/classroomio/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'ClassroomIO Help Docs',
   },
+  head: (
+    <>
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href={`/img/favicon.ico`}
+      />
+      <link
+        rel="icon"
+        type="image/x-icon"
+        sizes="16x16"
+        href={`/img/logo-16.png`}
+      />
+      <link
+        rel="icon"
+        type="image/x-icon"
+        sizes="32x32"
+        href={`/img/logo-32.png`}
+      />
+    </>
+  ),
 }
 
 export default config
